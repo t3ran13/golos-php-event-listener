f3e9899a9d89a1a7269b107a4f3a26b30905a3e4
t3ran13 <teran-555@yandex.ru>
2018-02-10 14:55:14 +0300

    upd DB manager

--- app/db/DBManagerInterface.php
+++ app/db/DBManagerInterface.php
@@ -5,7 +5,35 @@
 namespace GolosEventListener\app\db;
 
 
+use GolosEventListener\app\handlers\HandlerInterface;
+
 interface DBManagerInterface
 {
     public function connect();
+
+    /**
+     * add new event listener
+     *
+     * @param string                 $event
+     * @param HandlerInterface $handler
+     *
+     * @return void
+     */
+    public function addListener($event, HandlerInterface $handler);
+
+    /**
+     * remove event from listeners list
+     *
+     * @param string $event
+     *
+     * @return void
+     */
+    public function removeListener($event);
+
+    /**
+     * remove all events from listeners list
+     *
+     * @return void
+     */
+    public function clearListenersList();
 }
--- app/db/RedisManager.php
+++ app/db/RedisManager.php
@@ -5,10 +5,49 @@
 namespace GolosEventListener\app\db;
 
 
+use GolosEventListener\app\handlers\HandlerInterface;
+
 class RedisManager implements DBManagerInterface
 {
     public function connect()
     {
         // TODO: Implement connect() method.
     }
+
+    /**
+     * add new event listener
+     *
+     * @param string           $event
+     * @param HandlerInterface $handler
+     *
+     * @return void
+     */
+    public function addListener($event, HandlerInterface $handler)
+    {
+        // TODO: Implement addListener() method.
+    }
+
+    /**
+     * remove event from listeners list
+     *
+     * @param string $event
+     *
+     * @return void
+     */
+    public function removeListener($event)
+    {
+        // TODO: Implement removeListener() method.
+    }
+
+    /**
+     * remove all events from listeners list
+     *
+     * @return void
+     */
+    public function clearListenersList()
+    {
+        // TODO: Implement clearListenersList() method.
+    }
+
+
 }
