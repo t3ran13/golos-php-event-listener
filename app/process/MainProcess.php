d3d6a25f4061f6e391870e96cede81b3f7019276
t3ran13 <teran-555@yandex.ru>
2018-02-10 14:55:35 +0300

    pd MainProcess.php

--- app/process/MainProcess.php
+++ app/process/MainProcess.php
@@ -5,6 +5,7 @@
 namespace GolosEventListener\app\process;
 
 
+use GolosEventListener\app\AppConfig;
 use GolosEventListener\app\db\DBManagerInterface;
 
 class MainProcess extends ProcessAbstract
@@ -29,8 +30,23 @@ class MainProcess extends ProcessAbstract
         $this->db = $DBManager;
     }
 
+    /**
+     * update params and listeners list
+     */
+    public function init()
+    {
+        $listeners = $this->appConfig->getListenersList();
+        $this->db->clearListenersList();
+        foreach ($listeners as $event => $handler) {
+            $this->db->addListener($event, $handler);
+        }
+    }
+
+
     public function start()
     {
+        $this->init();
+
         //init connect to db
         // get listeners list
 
