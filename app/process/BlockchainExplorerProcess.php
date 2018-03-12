7478969d9247979f781c73d52755e9c5e6464d44
t3ran13 <teran-555@yandex.ru>
2018-03-12 15:55:11 +0300

    upd BlockchainExplorerProcess.php
    test data was added

--- app/process/BlockchainExplorerProcess.php
+++ app/process/BlockchainExplorerProcess.php
@@ -12,6 +12,16 @@ class BlockchainExplorerProcess extends ProcessAbstract
 {
     protected $priority = 10;
 
+    /**
+     * BlockchainExplorerProcess constructor.
+     *
+     * @param DBManagerInterface $DBManager
+     */
+    public function __construct(DBManagerInterface $DBManager)
+    {
+        $this->setDBManager($DBManager);
+    }
+
     public function initSignalsHandlers()
     {
         // TODO: Implement initSignalsHandlers() method.
@@ -20,13 +30,20 @@ class BlockchainExplorerProcess extends ProcessAbstract
     public function start()
     {
 //        pcntl_setpriority($this->priority);
+        echo PHP_EOL . ' BlockchainExplorer is running, info '
+            . print_r($this->getDBManager()->processInfoById($this->getId()), true);
+        $this->getDBManager()->processUpdateById($this->getId(), ['status' => 'running']);
 
         $n = 1;
         while (true) {
-            echo PHP_EOL . ($n++) . ' BlockchainExplorer is running,  pid ' . $this->getPid();
+
+            echo PHP_EOL . ($n++) . ' BlockchainExplorer is running, info '
+                . print_r($this->getDBManager()->processInfoById($this->getId()), true);
             if ($n >= 10) {
                 break;
             }
+
+            $this->getDBManager()->processUpdateById($this->getId(), ['last_update_datetime' => date('Y:m:d H:i:s')]);
             sleep(1);
         }
     }
