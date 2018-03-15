4a3df19ab05e02e2243fd535a37f6269ce9ee34a
t3ran13 <teran-555@yandex.ru>
2018-03-15 21:23:27 +0300

    upd MainProcess.php
    new functional

--- app/process/MainProcess.php
+++ app/process/MainProcess.php
@@ -11,6 +11,7 @@ use GolosEventListener\app\db\DBManagerInterface;
 class MainProcess extends ProcessAbstract
 {
     protected $priority = 20;
+    protected $isRunning = true;
     /**
      * @var null|AppConfig
      */
@@ -48,21 +49,51 @@ class MainProcess extends ProcessAbstract
             $this->getDBManager()->listenerAdd($event, $handler);
         }
 
+        $this->initSignalsHandlers();
 
-
-//        $processDBId = $this->getDBManager()->processAdd($this, ['status' => 'run']);
-//        $this->setId($processDBId);
 
         $this->getDBManager()->processesListClear();
+
+        //register main process in db
+        $processDBId = $this->getDBManager()->processAdd($this, ['status' => ProcessInterface::STATUS_RUNNING]);
+        $this->setId($processDBId);
+        $this->setStatus(ProcessInterface::STATUS_RUNNING);
+
+        //register processes in db
         foreach ($this->processesList as $process) {
-            $processDBId = $this->getDBManager()->processAdd($process, ['status' => 'run']);
+            $processDBId = $this->getDBManager()->processAdd($process, ['status' => ProcessInterface::STATUS_RUN]);
             $process->setId($processDBId);
         }
+
+        //add main process to processes list
+        array_unshift($this->processesList, $this);
     }
 
     public function initSignalsHandlers()
     {
-        // TODO: Implement initSignalsHandlers() method.
+        pcntl_signal(SIGTERM, [$this, 'signalsHandlers']);
+    }
+
+    public function signalsHandlers($signo, $pid = null, $status = null)
+    {
+        echo PHP_EOL . ' --- process with pid=' . $this->getPid() . ' got signal=' . $signo;
+
+        switch ($signo) {
+            case SIGTERM:
+                $this->setStatus(ProcessInterface::STATUS_STOP);
+                foreach ($this->processesList as $process) {
+                    if (in_array($process->getStatus(), ['run', 'running'])) {
+                        $processPid = $process->getPid();
+                        posix_kill($processPid, SIGTERM);
+                        echo PHP_EOL . ' --- to process with id=' . $processPid . ' sent signal ' . SIGTERM;
+//                        pcntl_waitpid($pid, $status);
+//                        echo PHP_EOL . ' --- from process with id=' . $process->getPid() . ' got signal ' . $status;
+                    }
+                }
+                $this->isRunning = false;
+                break;
+            default:
+        }
     }
 
 
@@ -73,57 +104,50 @@ class MainProcess extends ProcessAbstract
         $this->init();
 
         $n = 0;
-        while (true) {
+        while ($this->isRunning) {
+            echo PHP_EOL . '--- ' .($n++) . ' MainProcess is running';
+
+            $this->setLastUpdateDatetime(date('Y:m:d H:i:s'));
+
             $listProcessFromDB = $this->getDBManager()->processesListGet();
-//            echo PHP_EOL . ' --- list ' . count($listProcessFromDB);
 
-            foreach ($listProcessFromDB as $processId => $process) {
-                foreach ($this->processesList as $processObj) {
-//                        echo PHP_EOL . '$processObj ' . print_r($processObj, true);
-//                        echo PHP_EOL . '$processObj->getId() ' . $processObj->getId();
-//                        echo PHP_EOL . '$processId ' . $processId;
-//                    echo PHP_EOL . ' if ($processObj->getId() === $processId) { ' . $processObj->getId() .'/'. $processId;
-                    if ($processObj->getId() === $processId) {
-                        $processObj->setPid($process['pid']);
-                        $processObj->setStatus($process['status']);
-                        $processObj->setLastUpdateDatetime($process['last_update_datetime']);
+            foreach ($listProcessFromDB as $processId => $lastProcessInfo) {
+                /** @var ProcessInterface|null $processObj */
+                $processObj = null;
 
-                        echo PHP_EOL . ' --- process with id=' . $processId . ' is updated';
-
-                        continue(2);
+                foreach ($this->processesList as $process) {
+                    if ($process->getId() === $processId) {
+                        $processObj = $process;
+                        break;
                     }
+                }
 
-                    /** @var ProcessInterface $processObj */
+                if ($processObj === null) {
                     $dbClass = get_class($this->getDBManager());
-                    $processObj = new $process['handler'](new $dbClass);
+                    $processObj = new $lastProcessInfo['handler'](new $dbClass);
                     $processObj->setId($processId);
-                    $processObj->setPid($process['pid']);
-                    $processObj->setStatus($process['status']);
-                    $processObj->setLastUpdateDatetime($process['last_update_datetime']);
                     $this->processesList[] = $processObj;
 
                     echo PHP_EOL . ' --- ' . get_class($processObj) . ' is init';
                 }
             }
-//            echo PHP_EOL . ' --- ' . count($this->processesList) . ' counter';
+
             foreach ($this->processesList as $process) {
-//                echo PHP_EOL . '<pre>' . print_r($process, true) . '<pre>';
+                $status = $process->getStatus();
                 if (
-                    $process->getStatus() === 'run'
-                    && $process->getLastUpdateDatetime() === ''
+                    $status === 'run'
                 ) {
                     $pid = $this->forkProcess($process);
-                    $this->getDBManager()->processUpdateById($process->getId(), ['pid' => $pid]);
+                    $process->setPid($pid);
 
                     echo PHP_EOL . ' --- ' . get_class($process) . ' is started with id=' . $process->getId();
-                } elseif($process->getStatus() === 'stop') {
-//                    echo PHP_EOL . ' --- ' . get_class($process) . ' is running with pid ' . $process->getPid();
+                } elseif($status === 'stop') {
+                    echo PHP_EOL . ' --- to process with pid ' . $process->getPid() . ' was sent stop signal=' . SIGTERM;
+                    posix_kill($process->getPid(), SIGTERM);
                 }
             }
-            $n++;
-            if ($n >= 20) {
-                break;
-            }
+
+            pcntl_signal_dispatch();
             sleep(1);
         }
 
