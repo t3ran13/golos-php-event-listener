11618d95ecbfe91ff3593f0ad110e5c63a1dbbb3
t3ran13 <teran-555@yandex.ru>
2018-04-12 23:02:33 +0300

    upd MainProcess
    db init was moved, priority upd,new method, logical fixes

--- app/process/MainProcess.php
+++ app/process/MainProcess.php
@@ -10,7 +10,7 @@ use GolosEventListener\app\db\DBManagerInterface;
 
 class MainProcess extends ProcessAbstract
 {
-    protected $priority = -20;
+    protected $priority = 5;
     protected $isRunning = true;
     /**
      * @var null|AppConfig
@@ -32,10 +32,9 @@ class MainProcess extends ProcessAbstract
         $this->appConfig = $appConfig;
         $this->setDBManager($DBManager);
 
-        $dbClass = get_class($DBManager);
         $this->processesList = [
-            new BlockchainExplorerProcess(new $dbClass),
-            new EventsHandlersProcess(new $dbClass)
+            new BlockchainExplorerProcess($DBManager),
+            new EventsHandlersProcess($DBManager)
         ];
     }
 
@@ -71,11 +70,12 @@ class MainProcess extends ProcessAbstract
         //register main process in db
         $processDBId = $this->getDBManager()->processAdd($this, ['status' => ProcessInterface::STATUS_RUNNING]);
         $this->setId($processDBId);
-        $this->setStatus(ProcessInterface::STATUS_RUNNING);
+        $this->setPid(getmypid());
 
         //register processes in db
         foreach ($this->processesList as $process) {
             $processDBId = $this->getDBManager()->processAdd($process, ['status' => ProcessInterface::STATUS_RUN]);
+            $process->init();
             $process->setId($processDBId);
         }
 
@@ -85,7 +85,9 @@ class MainProcess extends ProcessAbstract
 
     public function initSignalsHandlers()
     {
-        pcntl_signal(SIGTERM, [$this, 'signalsHandlers']);
+        pcntl_signal(SIGTERM, [$this, 'signalsHandlers']); //kill
+        pcntl_signal(SIGINT, [$this, 'signalsHandlers']); //ctrl+c
+        pcntl_signal(SIGHUP, [$this, 'signalsHandlers']); //restart process
 //        pcntl_signal(SIGCHLD, [$this, 'signalsHandlers']);
     }
 
@@ -95,13 +97,14 @@ class MainProcess extends ProcessAbstract
             . print_r($signinfo, true);
 
         switch ($signo) {
+            case SIGINT:
             case SIGTERM:
                 $this->setStatus(ProcessInterface::STATUS_STOP);
                 foreach ($this->processesList as $process) {
                     if (in_array($process->getStatus(), ['run', 'running'])) {
                         $processPid = $process->getPid();
                         posix_kill($processPid, SIGTERM);
-                        echo PHP_EOL . ' --- to process with id=' . $processPid . ' sent signal ' . SIGTERM;
+                        echo PHP_EOL . ' --- to process with pid=' . $processPid . ' sent signal ' . SIGTERM;
 //                        pcntl_waitpid($pid, $status);
 //                        echo PHP_EOL . ' --- from process with id=' . $process->getPid() . ' got signal ' . $status;
                     }
@@ -115,10 +118,11 @@ class MainProcess extends ProcessAbstract
 
     public function start()
     {
+        pcntl_setpriority($this->priority, getmypid());
+
         echo PHP_EOL . ' --- ' . get_class($this) . ' is started';
-//        pcntl_setpriority($this->priority);
         $this->init();
-        $dbClass = get_class($this->getDBManager());
+//        $dbClass = get_class($this->getDBManager());
 
         $n = 0;
         while ($this->isRunning) {
@@ -128,6 +132,7 @@ class MainProcess extends ProcessAbstract
 
             $listProcessFromDB = $this->getDBManager()->processesListGet();
 
+            //get processes from db and init objects for new
             foreach ($listProcessFromDB as $processId => $lastProcessInfo) {
                 /** @var ProcessInterface|null $processObj */
                 $processObj = null;
@@ -140,7 +145,7 @@ class MainProcess extends ProcessAbstract
                 }
 
                 if ($processObj === null) {
-                    $processObj = new $lastProcessInfo['handler'](new $dbClass);
+                    $processObj = new $lastProcessInfo['handler']($this->getDBManager());
                     $processObj->init();
                     $processObj->setId($processId);
                     $this->processesList[] = $processObj;
@@ -149,40 +154,49 @@ class MainProcess extends ProcessAbstract
                 }
             }
 
+            //handle process statuses
             foreach ($this->processesList as $process) {
-                $status = $process->getStatus();
-                if (
-                    $status === ProcessInterface::STATUS_RUN
-                ) {
+                echo PHP_EOL . ' --- !!!!!' . ' pr=' . get_class($process) . ' st=' . $process->getStatus();
+                if ($process->isStartNeeded()) {
                     $pid = $this->forkProcess($process);
                     $process->setPid($pid);
 
                     echo PHP_EOL . ' --- PROCESS ID=' . $process->getId() . ' is started with pid=' . $pid;
-                } elseif($status === ProcessInterface::STATUS_STOP) {
+                } elseif($process->isStopNeeded()) {
                     echo PHP_EOL . ' --- to process with pid ' . $process->getPid() . ' was sent stop signal=' . SIGTERM;
                     posix_kill($process->getPid(), SIGTERM);
                 }
             }
 
 
+            sleep(1);
+
+            //get children signals
             $pid = 1;
             while ($pid > 0) {
-                $pid = pcntl_waitpid(-1, $status, WNOHANG);
+                $pid = pcntl_waitpid(-1, $pidStatus, WUNTRACED);
                 if ($pid > 0) {
-                    echo PHP_EOL . date('Y.m.d H:i:s') . ' process with pid=' . $this->getPid() . ' from child with pid=' . $pid . ' got status=' . $status;
+                    echo PHP_EOL . date('Y.m.d H:i:s') . ' process with pid=' . $this->getPid() . ' from child with pid=' . $pid . ' got status=' . $pidStatus;
 
-                    if(pcntl_wifexited($status)) {
-                        $code = pcntl_wexitstatus($status);
+                    if(pcntl_wifexited($pidStatus)) {
+                        $code = pcntl_wexitstatus($pidStatus);
                         print " and returned exit code: $code\n";
                     }
                     else {
-                        print " and was unnaturally terminated\n";
+                        print " and was unnaturally terminated, restart process\n";
+                        $process = null;
+                        foreach ($this->processesList as $processObj) {
+                            if ((int)$processObj->getPid() === $pid) {
+                                $process = $processObj;
+                                break;
+                            }
+                        }
+                        $process->setStatus(ProcessInterface::STATUS_RUN);
                     }
                 }
             }
 
             pcntl_signal_dispatch();
-            sleep(1);
         }
 
         //init connect to db
@@ -190,5 +204,17 @@ class MainProcess extends ProcessAbstract
         $this->setStatus(ProcessInterface::STATUS_STOPPED);
 
         echo PHP_EOL . ' -- end task of ' . get_class($this);
+    }
+
+    /**
+     * clear parent resourses in child process
+     *
+     * @return void
+     */
+    public function clearParentResources()
+    {
+//        unset($this->appConfig);
+//        unset($this->processesList);
+//        unset($this->dbManager);
     }
 }
